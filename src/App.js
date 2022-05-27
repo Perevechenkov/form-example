7c720b8d13d97ee8edd612b39f94875166833a72
DesperateHiking <perevechenkov_1999@mail.ru>
2022-05-27 16:31:52 +0500

    BasicForm & SimpleInput components w/ styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import BasicForm from './components/BasicForm';
+import SimpleInput from './components/SimpleInput';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className='app'>
+      <SimpleInput />
+      {/* <BasicForm /> */}
+    </div>
+  );
 }
 
 export default App;
